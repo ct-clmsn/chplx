6de029caa147b76068c58f3408f7f19d722c657d
ct-clmsn <ct.clmsn@gmail.com>
2023-09-03 07:44:38 -0400

    rollingback codegen modification on

--- backend/src/codegenvisitor.cpp
+++ backend/src/codegenvisitor.cpp
@@ -569,7 +569,7 @@ struct StatementVisitor {
       //auto const& rk = std::get<std::shared_ptr<range_kind>>(node->indexSet.kind);
       //auto & indices = rk->args;
 
-      os << "chplx::forLoop(HPX_CURRENT_SOURCE_LOCATION(), chplx::Range{";
+      os << "chplx::forLoop(chplx::Range{";
       if(node->indexSet.size() == 1) {
           ExprVisitor ev{os};
           std::visit(ev, node->indexSet[0]);
