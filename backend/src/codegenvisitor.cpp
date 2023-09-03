15967eaa3e06fdd0630d454d3a5e9b983954c82f
ct-clmsn <ct.clmsn@gmail.com>
2023-09-02 22:53:00 -0400

    updated codegenerator

--- backend/src/codegenvisitor.cpp
+++ backend/src/codegenvisitor.cpp
@@ -570,7 +570,7 @@ struct StatementVisitor {
       //auto const& rk = std::get<std::shared_ptr<range_kind>>(node->indexSet.kind);
       //auto & indices = rk->args;
 
-      os << "chplx::forLoop(chplx::Range{";
+      os << "chplx::forLoop(HPX_CURRENT_SOURCE_LOCATION(), chplx::Range{";
       if(node->indexSet.size() == 1) {
           ExprVisitor ev{os};
           std::visit(ev, node->indexSet[0]);
@@ -626,7 +626,7 @@ struct StatementVisitor {
       //auto const& rk = std::get<std::shared_ptr<range_kind>>(node->indexSet.kind);
       //auto & indices = rk->args;
 
-      os << "chplx::forall(chplx::Range{";
+      os << "chplx::forall(HPX_CURRENT_SOURCE_LOCATION(), chplx::Range{";
 
       if(node->indexSet.size() == 1) {
           ExprVisitor ev{os};
@@ -681,7 +681,7 @@ struct StatementVisitor {
       //auto const& rk = std::get<std::shared_ptr<range_kind>>(node->indexSet.kind);
       //auto & indices = rk->args;
 
-      os << "chplx::coforall(chplx::Range{";
+      os << "chplx::coforall(HPX_CURRENT_SOURCE_LOCATION(), chplx::Range{";
 /*
       if(std::holds_alternative<int_kind>(indices[0].kind)) {
          os << int_kind::value(indices[0].literal[0]);
