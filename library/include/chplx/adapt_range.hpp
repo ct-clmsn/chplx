f71fecc2d206eb6e4d2ec14717e13c88c20e8380
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2025-06-08 12:16:14 -0500

    Fix range  values
    Authored-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- library/include/chplx/adapt_range.hpp
+++ library/include/chplx/adapt_range.hpp
@@ -44,8 +44,9 @@ decltype(auto) iterate(Range<T, BoundedType, Stridable> const &r) noexcept {
 template <typename T>
 decltype(auto)
 iterate(Range<T, BoundedRangeType::bounded, false> const &r) noexcept {
-
-  return hpx::util::counting_shape(r.low(), r.high());
+  if (r.low() <= r.high())
+    return hpx::util::counting_shape(r.low(), r.high() + 1);
+  return hpx::util::counting_shape(r.low(), r.low());
 }
 
 } // namespace chplx
