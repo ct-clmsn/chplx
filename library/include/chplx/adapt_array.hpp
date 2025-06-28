f624a314fd2c4e65356e0d15c2da5c1573ae3e73
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2025-06-28 13:46:55 -0500

    Unwrap array iterators to pointers, if possible
    Signed-off-by: Hartmut Kaiser <hartmut.kaiser@gmail.com>

--- library/include/chplx/adapt_array.hpp
+++ library/include/chplx/adapt_array.hpp
@@ -14,8 +14,11 @@
 #include <hpx/generator.hpp>
 
 #include <hpx/iterator_support/iterator_range.hpp>
+#include <hpx/type_support/bit_cast.hpp>
+#include <hpx/type_support/is_contiguous_iterator.hpp>
 
 #include <iterator>
+#include <type_traits>
 
 namespace chplx {
 
@@ -44,15 +47,52 @@ decltype(auto) iterate(Array<T, Domain> &&a) noexcept {
   return iterate(detail::IteratorGenerator(std::move(a)));
 }
 
+namespace detail {
+
+template <typename Iter>
+  requires(!std::is_pointer_v<Iter>)
+HPX_FORCEINLINE constexpr auto get_unwrapped_ptr(Iter it) noexcept {
+
+  static_assert(hpx::traits::is_contiguous_iterator_v<Iter>,
+                "optimized merge is possible for contiguous-iterators "
+                "only");
+
+  using value_t = typename std::iterator_traits<Iter>::value_type;
+  return const_cast<value_t *>(hpx::bit_cast<value_t const volatile *>(&*it));
+}
+
+template <typename T>
+  requires(std::is_pointer_v<T>)
+HPX_FORCEINLINE constexpr auto get_unwrapped_ptr(T ptr) noexcept {
+  using value_t = std::remove_pointer_t<T>;
+  return const_cast<value_t *>(hpx::bit_cast<value_t const volatile *>(ptr));
+}
+
+template <typename Iter> HPX_FORCEINLINE auto get_unwrapped(Iter it) {
+
+  // is_contiguous_iterator_v is true for pointers
+  if constexpr (hpx::traits::is_contiguous_iterator_v<Iter>) {
+    return get_unwrapped_ptr(it);
+  } else {
+    return it;
+  }
+}
+
+} // namespace detail
+
 template <typename T>
 decltype(auto) iterate(Array<T, Domain<1>> const &a) noexcept {
 
-  return hpx::util::iterator_range(a.begin(), a.end());
+  auto begin = detail::get_unwrapped(a.begin());
+  auto end = detail::get_unwrapped(a.end());
+  return hpx::util::iterator_range(begin, end);
 }
 
 template <typename T> decltype(auto) iterate(Array<T, Domain<1>> &&a) noexcept {
 
-  return hpx::util::iterator_range(a.begin(), a.end());
+  auto begin = detail::get_unwrapped(a.begin());
+  auto end = detail::get_unwrapped(a.end());
+  return hpx::util::iterator_range(begin, end);
 }
 
 } // namespace chplx
